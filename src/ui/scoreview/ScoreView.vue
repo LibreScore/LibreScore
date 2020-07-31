a65f44c238d5d6be60ee1866db5c4e9cd94af801
Xmader <xmader@outlook.com>
2020-07-31 02:29:05 -0400

    feat: the sheet slides

--- src/ui/scoreview/ScoreView.vue
+++ src/ui/scoreview/ScoreView.vue
@@ -1,14 +1,26 @@
 <template>
-  <div>
-    <sheet-view
-      v-if="measures && img"
-      :page="page"
-      :measures="measures"
-      :img="img"
-      :currentTime="currentTime"
-      @seek="seek"
-    ></sheet-view>
-  </div>
+  <ion-slides
+    v-if="measures /** the score file is fully processed */"
+    ref="slides"
+    :scrollbar="true"
+    :pager="true"
+    @ionSlideDidChange="slideIndexChanged"
+  >
+    <ion-slide
+      v-for="(_, p) of imgUrls"
+      :key="'sheet-' + p /** slides are preallocated with the number of pages in this score */"
+    >
+      <div v-if="imgUrls[p] /** the sheet image of this page is processed */">
+        <sheet-view
+          :page="p"
+          :measures="measures"
+          :img="imgUrls[p]"
+          :currentTime="currentTime"
+          @seek="seek"
+        ></sheet-view>
+      </div>
+    </ion-slide>
+  </ion-slides>
 </template>
 
 <script lang="ts">
@@ -18,10 +30,13 @@ import { WebMscoreLoad, Measures } from '@/mscore'
 import type WebMscore from 'webmscore'
 import type { ScoreMetadata } from 'webmscore/schemas'
 
+import { IonSlides, IonSlide } from '@ionic/vue'
 import SheetView from './SheetView.vue'
 
 export default defineComponent({
   components: {
+    IonSlides,
+    IonSlide,
     SheetView,
   },
   props: {
@@ -37,36 +52,33 @@ export default defineComponent({
     return {
       mscore: null as any as WebMscore,
 
-      page: null as any as number, // The page index (0-based)
       measures: null as any as Measures,
       metadata: null as any as ScoreMetadata,
 
-      img: '',
+      imgUrls: null as any as string[],
       imgCache: new Map<number, Promise<string /** Blob URLs */>>(),
 
+      currentPage: null as any as number, // The current page index (0-based)
       currentTime: NaN, // The current playback time in ms
     }
   },
   watch: {
-    async page (current: number): Promise<void> {
+    currentPage (current: number): void {
       // get the maximum page index
       const max = this.metadata.pages - 1
+      const start = Math.max(current - 1, 0)
 
-      // preload up to 3 pages
-      for (let p = current; p <= Math.min(current + 2, max); p++) {
+      // preload up to 4 pages
+      for (let p = start; p <= Math.min(start + 3, max); p++) {
         void (
-          this.getPageImg(p).then((url) => {
-            console.info('preloaded page', p, url)
-          })
+          this.getPageImg(p)
         )
       }
-
-      this.img = await this.getPageImg(current)
     },
-    currentTime (): void {
+    async currentTime (): Promise<void> {
       if (!isFinite(this.currentTime)) { return }
       const currentEl = this.measures.getElByTime(this.currentTime)
-      this.page = currentEl.page
+      return this.slideTo(currentEl.page)
     },
   },
   methods: {
@@ -88,9 +100,19 @@ export default defineComponent({
 
       // avoid ...
       const blobUrlPromise = (async (): Promise<string> => {
+        // generate the svg image
         const svg = await this.mscore.saveSvg(page, true /** drawPageBackground */)
         const blob = new Blob([svg], { type: 'image/svg+xml' })
         const blobUrl = URL.createObjectURL(blob)
+
+        // update states
+        this.imgUrls[page] = blobUrl
+
+        if (process?.env?.NODE_ENV === 'development') {
+          // use 1-based index here (match the page number shown on the image)
+          console.info('loaded page #', page + 1, blobUrl)
+        }
+
         return blobUrl
       })()
 
@@ -101,6 +123,18 @@ export default defineComponent({
     },
     seek (time: number): void {
       this.currentTime = time
+    },
+    async slideIndexChanged (): Promise<void> {
+      const sidesEl = this.$refs.slides as any
+      const index = await sidesEl.getActiveIndex()
+      this.currentPage = index
+    },
+    /**
+     * Transition to the specified sheet page slide
+     */
+    slideTo (pageIndex: number): Promise<void> {
+      const sidesEl = this.$refs.slides as any
+      return sidesEl.slideTo(pageIndex, 0 /* speed */) // also changes `this.currentPage`
     },
   },
   async mounted () {
@@ -115,10 +149,12 @@ export default defineComponent({
 
     // get the score metadata
     this.metadata = await mscore.metadata()
+    // preallocate the `imgUrls` array for Vue list rendering
+    this.imgUrls = Array.from({ length: this.metadata.pages })
 
     // load sheet images
-    // trigger the `page` watcher
-    this.page = 0
+    // trigger the `currentPage` watcher
+    this.currentPage = 0
 
     // get the positions of measures
     const mpos = await mscore.measurePositions()
