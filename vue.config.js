7f433b2c78a7e842603465b21ffdfbad2921c569
Xmader <xmader@outlook.com>
2020-08-04 11:26:01 -0400

    feat: LibreScore logo

--- vue.config.js
+++ vue.config.js
@@ -4,4 +4,29 @@ module.exports = {
   filenameHashing: true,
   productionSourceMap: false,
   integrity: true,
+  pwa: {
+    name: 'LibreScore',
+    themeColor: '#50C8FF',
+    manifestOptions: {
+      icons: [
+        {
+          src: './img/icons/logo-192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: './img/icons/logo-512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+    },
+    iconPaths: {
+      favicon32: 'img/icons/logo-32.png',
+      favicon16: null,
+      maskIcon: null,
+      appleTouchIcon: 'img/icons/logo.svg',
+      msTileImage: 'img/icons/logo.svg',
+    },
+  },
 }
