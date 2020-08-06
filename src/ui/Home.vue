63b0ba753b5b2fa5400cbd17dad53a142ff6b663
Xmader <xmader@outlook.com>
2020-08-06 06:47:54 -0400

    feat: home page (empty)

---
+++ src/ui/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data () {
+    return {
+    }
+  },
+})
+</script>
