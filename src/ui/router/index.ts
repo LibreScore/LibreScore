2cac6ef59866141659fa479e251620eae6fe4db6
Xmader <xmader@outlook.com>
2020-12-07 18:15:00 -0500

    fix: H5 history routing on IPFS HTTP Gateways

--- src/ui/router/index.ts
+++ src/ui/router/index.ts
@@ -27,8 +27,21 @@ const routes: RouteRecordRaw[] = [
   },
 ]
 
+/**
+ * Workaround for HTML5 history routing on IPFS HTTP Gateways  
+ * see @/loader.js
+ */
+const getRouterBase = (pathname: string = location?.pathname): string | undefined => {
+  const l = pathname.split('/')
+  // ['', 'ipfs', 'QmHash...', ...]
+  const isOnGateway = l.length >= 4 && ['ipfs', 'ipns'].includes(l[1])
+  if (isOnGateway) {
+    return l.slice(0, 3).join('/')
+  }
+}
+
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(getRouterBase()),
   routes,
 })
 
